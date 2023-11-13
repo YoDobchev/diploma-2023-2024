ed52da8e9e516bfcb2df908fd6d0e09169986703
y.dobchev <y.dobchev@myxrobotics.com>
2023-11-13 10:41:35 +0200

    base

---
+++ index.ts
@@ -0,0 +1,20 @@
+import Jimp from 'jimp';
+
+
+const options = [
+    // {apply: "hue", params: [70] },
+    { apply: "red", params: [10] },
+    { apply: "brighten", params: [-5] },
+];
+
+Jimp.read("img/input.jpg", (err: any, lenna: any) => {
+    if (err) throw err;
+    lenna
+        .color(options)
+        // .pixelate(2)
+        // .resize(256, 256) // resize
+        // .quality(60) // set JPEG quality
+        // .greyscale() // set greyscale
+        .write("img/output.jpg"); // save
+});
+console.log("lol")
